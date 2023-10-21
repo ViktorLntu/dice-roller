51f3633ddd269ddfb6a867d4a757d39e78911853
Viktor Stashchuk <vstashchuk@reenbit.com>
2023-10-21 12:52:47 +0300

    Write unit test to check 'roll' method

--- app/src/test/java/com/example/diceroller/ExampleUnitTest.kt
+++ app/src/test/java/com/example/diceroller/ExampleUnitTest.kt
@@ -1,17 +1,39 @@
 package com.example.diceroller
 
 import org.junit.Test
-
 import org.junit.Assert.*
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
+import org.junit.runners.Parameterized.Parameters
+
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-class ExampleUnitTest {
+@RunWith(Parameterized::class)
+class ExampleUnitTest(private val numberOfSides: Int) {
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun generates_number() {
+        val dice = Dice(numberOfSides)
+        val rollResult = dice.roll()
+
+        assertTrue("The value of rollResult was not between 1 and ${numberOfSides}", rollResult in 1..numberOfSides)
+    }
+
+    companion object {
+        @Parameters
+        @JvmStatic
+        fun rollValues(): Collection<Array<Int>> {
+            return listOf(
+                arrayOf(6),
+                arrayOf(12),
+                arrayOf(25),
+                arrayOf(37),
+                arrayOf(15),
+                arrayOf(49)
+            )
+        }
     }
 }
